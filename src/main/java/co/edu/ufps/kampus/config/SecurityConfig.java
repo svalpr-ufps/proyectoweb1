0cc0ecebd9353197b3465098d759edfafb18703e
Val Parada <sarithvalentinapr@ufps.edu.co>
2025-05-28 18:37:30 -0500

    fix

--- src/main/java/co/edu/ufps/kampus/config/SecurityConfig.java
+++ src/main/java/co/edu/ufps/kampus/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import co.edu.ufps.kampus.services.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
