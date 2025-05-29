a2ad7988501af8545b77295405d33c8ef3bc3e5b
Val Parada <sarithvalentinapr@ufps.edu.co>
2025-05-28 19:24:13 -0500

    adding methods + one attribute

--- src/main/java/co/edu/ufps/kampus/repositories/ReservationRepository.java
+++ src/main/java/co/edu/ufps/kampus/repositories/ReservationRepository.java
@@ -1,6 +1,9 @@
 package co.edu.ufps.kampus.repositories;
 
 import co.edu.ufps.kampus.entities.Reservation;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
@@ -14,4 +17,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, UUID> 
     List<Reservation> findByStartDateBetween(LocalDateTime start, LocalDateTime end);
     boolean existsByResourceIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(
             UUID resourceId, LocalDateTime end, LocalDateTime start);
+
+    boolean existsByRoomIdAndStartDateLessThanEqualAndEndDateGreaterThanEqual(@NotNull(message = "Room ID is required") UUID roomId, @NotNull(message = "End date/time is required") @Future(message = "End date must be in the future") LocalDateTime endDate, @NotNull(message = "Start date/time is required") @FutureOrPresent(message = "Start date must be in the present or future") LocalDateTime startDate);
 }
